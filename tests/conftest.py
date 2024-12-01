ec76425eb7b082cd3b2b49cfa4289e92b4cf15b3
PeterLombaers <peter@idfuse.nl>
2024-12-01 17:06:31 +0100

    Add fixtures and tests for rating type routes

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,10 +1,13 @@
 from typing import Any, Generator
+
+import factory
 import pytest
 from fastapi.testclient import TestClient
 from sqlmodel import Session, SQLModel, create_engine
 from sqlmodel.pool import StaticPool
 
 from backend.main import app, get_session
+from backend.models import RatingType
 
 
 @pytest.fixture
@@ -18,12 +21,44 @@ def session() -> Generator[Session, Any, None]:
 
 
 @pytest.fixture
-def client(session: Session) -> Generator[TestClient, Any, None]:  
-    def get_session_override():  
+def client(session: Session) -> Generator[TestClient, Any, None]:
+    def get_session_override():
         return session
 
-    app.dependency_overrides[get_session] = get_session_override  
+    app.dependency_overrides[get_session] = get_session_override
 
-    client = TestClient(app)  
-    yield client  
-    app.dependency_overrides.clear()  
+    client = TestClient(app)
+    yield client
+    app.dependency_overrides.clear()
+
+
+def RatingTypeFactory(session):
+    class RatingTypeFactory(factory.alchemy.SQLAlchemyModelFactory):
+        class Meta:
+            model = RatingType
+            sqlalchemy_session = session
+
+        name = "interne"
+
+    return RatingTypeFactory
+
+
+@pytest.fixture
+def rating_type(session: Session):
+    yield RatingTypeFactory(session)()
+
+
+@pytest.fixture
+def rating_type_factory(session: Session):
+    yield RatingTypeFactory(session=session)
+
+
+# @pytest.fixture
+# def rating_type(session: Session) -> Generator[RatingType, Any, None]:
+#     rating_type = RatingType(name="interne_rating")
+#     session.add(rating_type)
+#     session.commit()
+#     session.refresh(rating_type)
+#     yield rating_type
+#     session.delete(rating_type)
+#     session.commit()
--- tests/test_api.py
+++ tests/test_api.py
@@ -1,6 +1,8 @@
-from sqlmodel import Session, select
-from backend.models import RatingType
+from fastapi.encoders import jsonable_encoder
 from fastapi.testclient import TestClient
+from sqlmodel import Session, select
+
+from backend.models import RatingType
 
 
 def test_create_rating_type(session: Session, client: TestClient):
@@ -11,3 +13,32 @@ def test_create_rating_type(session: Session, client: TestClient):
     assert rating_type.name == "interne_rating"
     for key in {"id", "created_at", "updated_at"}:
         assert key in dict(rating_type)
+
+
+def test_get_rating_type(rating_type: RatingType, client: TestClient):
+    response = client.get(f"/rating_types/{rating_type.name}/")
+    response.raise_for_status()
+    res_rating_type = response.json()
+    assert res_rating_type == jsonable_encoder(rating_type)
+
+
+def test_list_rating_types(rating_type_factory, client):
+    r0, r1 = rating_type_factory(name="r0"), rating_type_factory(name="r1")
+    response = client.get("/rating_types/")
+    response.raise_for_status()
+    rating_types = response.json()
+    assert len(rating_types) == 2
+    assert jsonable_encoder(r0) == rating_types[0]
+    assert jsonable_encoder(r1) == rating_types[1]
+
+
+def test_update_rating_type(rating_type, client, session):
+    new_name = "foo"
+    client.patch(f"/rating_types/{rating_type.name}/", json={"name": new_name})
+    session.refresh(rating_type)
+    assert rating_type.name == new_name
+
+
+def test_delete_rating_type(rating_type, client, session):
+    client.delete(f"/rating_types/{rating_type.name}/")
+    assert len(session.scalars(select(RatingType)).all()) == 0
