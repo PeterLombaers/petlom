82363d83c537862a605fc28e61b7eb55bee1c6a0
PeterLombaers <peter@idfuse.nl>
2024-12-25 21:33:35 +0100

    Add competition type

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,22 @@
+import { Competition, CompetitionProps } from "./Competition";
+
+const competitions: CompetitionProps[] = [
+  {
+    name: "interne_2024",
+    type: "simkro",
+    created_at: new Date("2024-12-21T13:44:57.875553"),
+    updated_at: new Date("2024-12-21T13:44:57.875670"),
+  },
+];
+
 function App() {
   return (
     <div>
       <h2>PetLom</h2>
       <h4>Manage Chess Competitions</h4>
+      {competitions.map((competition) => {
+        return <Competition {...competition} />;
+      })}
     </div>
   );
 }
---
+++ frontend/src/Competition.tsx
@@ -0,0 +1,19 @@
+export type CompetitionProps = {
+  name: string;
+  type: "simkro";
+  created_at: Date;
+  updated_at: Date;
+  matches?: any[];
+};
+
+export function Competition(props: CompetitionProps) {
+  const { name, type, created_at, updated_at } = props;
+  return (
+    <article>
+      <h2>{name}</h2>
+      <p>Type: {type}</p>
+      <p>Last Updated: {updated_at.toISOString()}</p>
+      <p>Created: {created_at.toISOString()}</p>
+    </article>
+  );
+}
