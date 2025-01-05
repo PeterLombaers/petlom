289d4a70423d346485023f79bf2fb7746499d6be
PeterLombaers <peter@idfuse.nl>
2025-01-05 14:00:59 +0100

    Improve Layout

--- frontend/src/Layout.tsx
+++ frontend/src/Layout.tsx
@@ -1,17 +1,73 @@
-import { Typography } from "@mui/material";
+import {
+  AppBar,
+  Box,
+  Divider,
+  Drawer,
+  List,
+  ListItem,
+  ListItemButton,
+  ListItemIcon,
+  ListItemText,
+  Toolbar,
+  Typography,
+} from "@mui/material";
 import { Outlet } from "react-router-dom";
+import EmojiEventsIcon from "@mui/icons-material/EmojiEvents";
+import PeopleIcon from "@mui/icons-material/People";
+
+const drawerWidth = 240;
 
 export default function Layout() {
   return (
-    <>
-      <header>
-        <Typography variant="h2" align="center">
-          PetLom
-        </Typography>
-      </header>
-      <main>
+    <Box sx={{ display: "flex" }}>
+      <AppBar
+        position="fixed"
+        sx={{ zIndex: (theme) => theme.zIndex.drawer + 1 }}
+      >
+        <Toolbar>
+          <Typography variant="h6" noWrap component="div">
+            PetLom
+          </Typography>
+        </Toolbar>
+      </AppBar>
+      <Drawer
+        variant="permanent"
+        sx={{
+          width: drawerWidth,
+          flexShrink: 0,
+          [`& .MuiDrawer-paper`]: {
+            width: drawerWidth,
+            boxSizing: "border-box",
+          },
+        }}
+      >
+        <Toolbar />
+        <Box sx={{ overflow: "auto" }}>
+          <List>
+            <ListItem disablePadding>
+              <ListItemButton href="/competitions">
+                <ListItemIcon>
+                  <EmojiEventsIcon />
+                </ListItemIcon>
+                <ListItemText primary="Competitions" />
+              </ListItemButton>
+            </ListItem>
+            <ListItem disablePadding>
+              <ListItemButton href="/players">
+                <ListItemIcon>
+                  <PeopleIcon />
+                </ListItemIcon>
+                <ListItemText primary="Players" />
+              </ListItemButton>
+            </ListItem>
+          </List>
+        </Box>
+        <Divider />
+      </Drawer>
+      <Box component="main" sx={{ flexGrow: 1, p: 3 }}>
+        <Toolbar />
         <Outlet />
-      </main>
-    </>
+      </Box>
+    </Box>
   );
 }
