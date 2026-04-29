bddc754d6bd12694d49f837cbe6b3d437038e136
Peter Lombaers <peter@idfuse.nl>
2026-04-29 12:07:04 +0200

    Format

--- backend/routers/players.py
+++ backend/routers/players.py
@@ -7,7 +7,13 @@ from sqlmodel import select
 
 from backend.auth import ModeratorDep
 from backend.dependencies import MAX_PAGE_LENGTH, SessionDep, find_object
-from backend.models import ExternalRating, Player, PlayerCreate, PlayerPublic, PlayerUpdate
+from backend.models import (
+    ExternalRating,
+    Player,
+    PlayerCreate,
+    PlayerPublic,
+    PlayerUpdate,
+)
 
 router = APIRouter(prefix="/players", tags=["players"])
 
