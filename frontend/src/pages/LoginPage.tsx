773c328f7343c7fa6d4e082bc2d21799a2c44106
Peter Lombaers <peter@idfuse.nl>
2026-05-01 21:51:15 +0200

    Format

--- frontend/src/pages/LoginPage.tsx
+++ frontend/src/pages/LoginPage.tsx
@@ -66,7 +66,13 @@ export default function LoginPage() {
               onChange={(e) => setPassword(e.target.value)}
               required
             />
-            <Button type="submit" variant="filled" loading={loading} fullWidth mt="xs">
+            <Button
+              type="submit"
+              variant="filled"
+              loading={loading}
+              fullWidth
+              mt="xs"
+            >
               Login
             </Button>
           </Stack>
